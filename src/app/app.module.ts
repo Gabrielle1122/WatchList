1ed2076cf35fe6e76843442bec85640cfbcdafd4
Gabrielle <gabbiezhou@gmail.com>
2019-09-15 20:32:10 -0400

    add media-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MediaItemComponent } from './media-item/media-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  //all of these can be set up as an array
+
+  //the declarations property is used to make components, directives
+  //pipes available to your module, that don't come from another module
+  declarations: [AppComponent, MediaItemComponent],
+  //imports property is used to bring in other angular modules
+  //your module will need
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
+  //the bootstrap property is used for a root module, and will let angular knnow
+  //which component or components will be the starting points for the bootstrap process
+  //entry point for your app code
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
