b4f605204ec12a31144a895c0318806ba0b2f71c
Gabrielle <gabbiezhou@gmail.com>
2019-09-16 13:46:36 -0400

    add media item list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MediaItemComponent } from './media-item/media-item.component';
+import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 
 @NgModule({
   //all of these can be set up as an array
 
   //the declarations property is used to make components, directives
   //pipes available to your module, that don't come from another module
-  declarations: [AppComponent, MediaItemComponent],
+  declarations: [AppComponent, MediaItemComponent, MediaItemListComponent],
   //imports property is used to bring in other angular modules
   //your module will need
   imports: [BrowserModule, AppRoutingModule],
