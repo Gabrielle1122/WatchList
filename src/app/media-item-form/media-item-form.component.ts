a2cbe7377cd86812a7b74ecb2980f5e3747c314e
Gabrielle <romansakura@live.cn>
2019-09-17 16:02:09 -0400

    error handle

--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -59,6 +59,6 @@ export class MediaItemFormComponent implements OnInit {
 
   onSubmit(mediaItem) {
     console.log(mediaItem);
-    this.mediaItemService.add(mediaItem);
+    this.mediaItemService.add(mediaItem).subscribe();
   }
 }
